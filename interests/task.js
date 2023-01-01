844095540613d1901fc48ce4dde011d484d19356
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-01-01 17:44:19 +0300

    32

--- interests/task.js
+++ interests/task.js
@@ -12,7 +12,11 @@ mainCheckOne.onclick = function () {
 
   for (let i = 0; i < firstBlock.length; i++) {
     let elements = firstBlock[i].querySelectorAll(".interest__check");
+    if(elements[0].checked == false){
     elements[0].checked = true;
+    } else {
+       elements[0].checked = false;
+    }    
   }
 };
 
@@ -25,7 +29,11 @@ mainCheckTwo.onclick = function () {
 
   for (let i = 0; i < secondtBlock.length; i++) {
     let elements = secondtBlock[i].querySelectorAll(".interest__check");
-    elements[0].checked = true;
+    if (elements[0].checked == false) {
+      elements[0].checked = true;
+    } else {
+      elements[0].checked = false;
+    }   
   }
 };
 
@@ -34,38 +42,3 @@ mainCheckTwo.onclick = function () {
 
 
 
-
-
-/*
-for (let i=0;i<boxOfInterests.length;i++){
-   
-  if(boxOfInterests[i].closest(".iresntets_active")==null){
-
-    let thingsOfSelect = Array.from(boxOfInterests[i].closest(".interest").querySelectorAll(".interest__check"));
-    
-    boxOfInterests[i].onclick = function () {
-      let firstBlock = mainCheckOne
-        .closest(".interest")
-        .querySelector(".interests_active")
-        .querySelectorAll(".interest");
-
-      for (let i = 0; i < firstBlock.length; i++) {
-        let elements = firstBlock[i].querySelectorAll(".interest__check");
-        elements[0].checked = true;
-      }
-    };
-  }
-}
-
- /*thingsOfSelect.forEach((el) => {
-       if (el.checked == false) {
-         el.checked = "true";
-       } else if (el.checked == "true") {
-         console.log(333);
-         el.removeAttribute("checked");
-         el.checked = "false";
-       }
-     });*/
-
-// boxOfInterests[i].onclick = function (){
-// let thingsOfSelect = boxOfInterests[i].closest(".interest").querySelectorAll(".interest__check");*/
